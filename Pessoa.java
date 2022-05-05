c1ebc89270c13f691f36b6f390a86fd7e70e06c4
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-05 19:06:38 -0300

    Update Pessoa.java

--- Pessoa.java
+++ Pessoa.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package objetos6;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author cadua
- */
 public class Pessoa {
     public ArrayList<Conta>contas = new ArrayList<>(); //Inicializa variável conta do tipo ArrayList
     public ArrayList<Carro>carros = new ArrayList<>();
