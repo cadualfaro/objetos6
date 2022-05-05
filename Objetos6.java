01efd56066a4b2cd7eeb203a5d0409da81f6204c
Carlos Eduardo da Trindade Alfaro <101533861+cadualfaro@users.noreply.github.com>
2022-05-05 19:05:27 -0300

    Update Objetos6.java

--- Objetos6.java
+++ Objetos6.java
@@ -1,24 +1,3 @@
-/*
-
-Pessoa pode possuir 2 contas. Inicialmente não tem nenhuma conta. Criar método registrarConta.
-Verificar em qual atributo registrar. Verificar se a Pessoa já não tem 2 contas
-Conta só tem um dono
-
-Modifique o código da Pessoa com duas Contas para funcionar com qualquer número de contas
-Modifique o método getSaldoTotal( ), que retorna o valor somado de todas as contas da pessoa
-
-Como contabilizar quantos depósitos e quantos saques foram feitos em uma conta? Adapte a classe Conta
-para conseguirmos saber a quantidade de depósitos/saques.
-Crie um método (no projeto principal) que compara se o nome de duas pessoas é igual. 
-Dica: use o método ‘equals’ da classe String
-
-Implemente que cada Pessoa possa ter vários objetos do tipo Carro
-Implemente os seguintes métodos dentro da classe Pessoa
--getMaisRapido(); retorna o objeto Carro mais rápido que a pessoa tem
--getMaisAntigo(); retorna o objeto Carro mais antigo que a pessoa tem
-
-Entregar códigos (Pessoa.java, Carro.java e Conta.java) em um zip
- */
 package objetos6;
 
 import java.util.Scanner;
